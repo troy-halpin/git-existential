b1e858fb80e874c08c3eae27885208e7dcb1f0e1
Troy Halpin <troy.halpin@procore.com>
2019-05-28 22:25:07 -0700

    add philosopher quote button, display either git message or philosopher

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,52 @@
 import * as React from 'react'
 import './App.css'
-const existential_data = require('./devs_gazing_into_the_abyss.json')
+const existential_devs = require('./data/devs_gazing_into_the_abyss.json')
+const existential_philosphers = require('./data/philosophers_gazing_into_the_abyss.json')
 
 interface AppProps {}
 
-const getRandomValue = () => Math.floor(existential_data.length * Math.random())
+const getRandomValue = (length: number) => Math.floor(length * Math.random())
 
 export const App: React.FC<AppProps> = () => {
-  const [index, setIndex] = React.useState(getRandomValue())
+  const [index, setIndex] = React.useState(
+    getRandomValue(existential_devs.length)
+  )
+  const [isDev, setIsDev] = React.useState(true)
   return (
     <div className="app">
-      <div>{existential_data[index].subject}</div>
-      <div>{`-${existential_data[index].name}`}</div>
-      <button
-        onClick={() => setIndex(getRandomValue())}
-        style={{ margin: '8px', padding: '8px' }}
-      >
-        Git Existential
-      </button>
+      <div className="message">
+        {isDev ? (
+          <>
+            <div>{existential_devs[index].subject}</div>
+            <div>{`-${existential_devs[index].name}`}</div>
+          </>
+        ) : (
+          <>
+            <div>{existential_philosphers[index].quote}</div>
+            <div>{`-${existential_philosphers[index].author}`}</div>
+          </>
+        )}
+      </div>
+      <div className="button-list">
+        <button
+          className="button"
+          onClick={() => {
+            setIndex(getRandomValue(existential_devs.length))
+            setIsDev(true)
+          }}
+        >
+          Git Existential
+        </button>
+        <button
+          className="button"
+          onClick={() => {
+            setIndex(getRandomValue(existential_philosphers.length))
+            setIsDev(false)
+          }}
+        >
+          Get Existential
+        </button>
+      </div>
     </div>
   )
 }
